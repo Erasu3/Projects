9fd4146bbb09981ef16b3ed26b023086232ccd8b
Erasu3 <tompla@ukr.net>
2015-03-05 15:30:07 +0200

    Create 002.cpp

---
+++ 002.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Solution 2
+//
+//  Created by  Lysogor Dima on 04.03.15.
+//  Copyright (c) 2015  Lysogor Dima. All rights reserved.
+//
+
+#include <stdio.h>
+
+int Arr[100];
+
+int main()
+{
+    Arr[0] = 1;
+    Arr[1] = 1;
+    int sum = 0;
+    for (int i = 2;i < 100 ; i++){
+        Arr[i] = Arr[i-1] + Arr[i-2];
+        if (Arr[i] > 4000000)
+            break;
+        if (Arr[i]%2 == 0)
+            sum +=Arr[i];
+        printf("%d\n",Arr[i]);
+    }
+    printf("%d\n",sum);
+    return 0;
+}
